a916aded41257aed76e8b2deb44638baca729e64
Joelle Everaert <72984240+Joelle-Everaert@users.noreply.github.com>
2020-12-15 08:56:56 +0100

    V1

---
+++ README.md
@@ -0,0 +1,35 @@
+# Quizz
+
+* Solo project made by me [Joelle Everaert](https://github.com/Joelle-Everaert)
+
+
+* Date : 14/12/2020
+
+
+## What is this ?
+It is a solo work as part of my training at **[BeCode](https://becode.org)**. <br>
+
+[Trello](https://trello.com/b/iMYkjSat/restaurant)
+
+
+## Context  
+As part of our training, we finish the front end with 4 different projects. This is project 4. </br>
+We had to create a quiz using an API. 
+## Purpose?
+Putting our knowledge and cohesion into practice through teamwork.
+
+## Languages used?
+* HTML
+* CSS
+    * Flexbox
+* Javascript
+    
+## Improvement
+* 
+
+## Overview 
+[![little overview](Overview.png)]() 
+
+### Original instruction
+
+[Link](https://github.com/becodeorg/bxl-hopper-1-25/tree/master/The%20Hill/projects/99.leaving_the_hills)
---
+++ script.js
@@ -0,0 +1,105 @@
+const section = document.querySelector('.quizz')
+
+// copie stack overflow
+const shuffle = (array) => {
+  let j, x, i;
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = array[i];
+    array[i] = array[j];
+    array[j] = x;
+  }
+  answers = array
+}
+
+fetch('https://opentdb.com/api.php?amount=15&category=14&difficulty=medium&type=multiple')
+  .then(response => response.json())
+  .then((quizzApi) => {
+    console.log(quizzApi)
+
+    for (let element of quizzApi.results) {
+      let answers = []
+      let correctAnswer = element.correct_answer
+      const question = document.createElement('div')
+      question.className = 'question'
+      question.innerHTML = element.question
+      const possibility = document.createElement('div')
+      possibility.className = 'possibility'
+      answers.push(element.correct_answer)
+      for (let elem of element.incorrect_answers) {
+        answers.push(elem)
+      }
+
+      shuffle(answers);  // shuffle nos réponse de notre array 
+
+      // let i = 0
+
+      let name = element.question // pour que name dans checkboxanswer soit associé à la question, un name par question 
+      for (let el of answers) {
+        const divGeneral = document.createElement('div')
+        divGeneral.className = 'divGeneral'
+
+        const checkboxAnswers = document.createElement('input') // on doit mettre le meme nom a la radio pour qu'il y en ai que un de selctionné
+        checkboxAnswers.setAttribute('type', 'radio')
+        checkboxAnswers.setAttribute('name', name)
+
+        const checkboxAnswersLabel = document.createElement('label')
+        checkboxAnswersLabel.innerHTML = el
+
+        divGeneral.appendChild(checkboxAnswers)
+        divGeneral.appendChild(checkboxAnswersLabel)
+        possibility.appendChild(divGeneral)
+        possibility.appendChild(checkboxAnswers)
+        possibility.appendChild(checkboxAnswersLabel)
+
+        
+        checkboxAnswers.addEventListener('click', function () {
+          let labels = document.querySelectorAll('label')
+          for (let e of labels) {
+            e.classList.remove('green')
+            e.classList.remove('red')
+          }
+          if (checkboxAnswersLabel.innerHTML == correctAnswer) { // veut le contenu texte car ligne 21 = a une balise html
+            checkboxAnswersLabel.classList.add('green')
+            // i += 1
+            // const score = document.querySelector('.score')
+            // score.innerHTML = 'Score : ' + i
+          } else {
+            checkboxAnswersLabel.classList.add('red')
+          }
+        })
+      }
+      section.appendChild(question)
+      section.appendChild(possibility)
+
+    }
+
+  })
+  .catch(error => {
+    console.log('There was an error!', error)
+  })
+
+const minuteur = document.querySelector('.minuteur')
+const timer = document.querySelector('.timer');
+minuteur.appendChild(timer)
+
+
+let sec = 150;
+
+let interval = setInterval(function () {
+  sec--;
+  let min = Math.floor(sec / 60) // 3min mais reste 45sec
+  timer.innerHTML = min + ' min ' + (sec - (min * 60)) + ' sec' // on retire le nombre de min(3) de mes secondes
+  if (sec == 10) {
+    timer.classList.add('redText')
+  }
+  if(sec == 0){
+    clearInterval(interval)
+    // setTimeout(function () { //effectue une action
+      alert('LOOSER --> TRY AGAIN :) ')
+      window.location = ''
+  
+    // }, 1000);
+  }
+
+}, 1000);
